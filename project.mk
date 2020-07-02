b3add9db031c82095708a36ae6e84060856c55d1
Ravi Trivedi <travi@redhat.com>
2020-07-02 16:13:41 +0530

    Fixing image repo name from USER to app-sre

--- project.mk
+++ project.mk
@@ -3,7 +3,7 @@ OPERATOR_NAME?=managed-upgrade-operator
 OPERATOR_NAMESPACE?=managed-upgrade-operator
 
 IMAGE_REGISTRY?=quay.io
-IMAGE_REPOSITORY?=$(USER)
+IMAGE_REPOSITORY?=app-sre
 IMAGE_NAME?=$(OPERATOR_NAME)
 
 VERSION_MAJOR?=0
