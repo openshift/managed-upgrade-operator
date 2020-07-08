90b566a3f1512d538f4d94d5e74273af1da2a4af
Ravi Trivedi <travi@redhat.com>
2020-07-08 17:48:55 +0530

    Dummy change to test app-sre pipeline

--- hack/app_sre_build_deploy.sh
+++ hack/app_sre_build_deploy.sh
@@ -30,5 +30,5 @@ skopeo copy --dest-creds "${QUAY_USER}:${QUAY_TOKEN}" \
 "$CURRENT_DIR"/app_sre_create_image_catalog.sh staging "$QUAY_IMAGE"
 
 # create and push production image catalog
-## Disabling production image pruning for now as it's not required
+### Disabling production image pruning for now as it's not required
 REMOVE_UNDEPLOYED=false "$CURRENT_DIR"/app_sre_create_image_catalog.sh production "$QUAY_IMAGE"
