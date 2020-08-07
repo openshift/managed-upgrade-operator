25b921a20cdeaafda8c70600348511b66fd120b7
John Roche <jroche@redhat.com>
2020-08-07 10:27:10 +1000

    Externalise control plane critical alerts

--- pkg/osd_cluster_upgrader/config.go
+++ pkg/osd_cluster_upgrader/config.go
@@ -12,8 +12,35 @@ type osdUpgradeConfig struct {
 }
 
 type maintenanceConfig struct {
-	ControlPlaneTime int `yaml:"controlPlaneTime"`
-	WorkerNodeTime   int `yaml:"workerNodeTime"`
+	ControlPlaneTime int           `yaml:"controlPlaneTime"`
+	WorkerNodeTime   int           `yaml:"workerNodeTime"`
+	IgnoredAlerts    ignoredAlerts `yaml:"ignoredAlerts"`
+}
+
+type ignoredAlerts struct {
+	// Generally upgrades should not fire critical alerts but there are some critical alerts that will fire.
+	// e.g. 'etcdMembersDown' happens as the masters drain/reboot and a master is offline but this is expected and will resolve.
+	// This is a list of critical alerts that can be ignored while upgrading of controlplane occurs
+	ControlPlaneCriticals []string `yaml:"controlPlaneCriticals"`
+}
+
+func (cfg *maintenanceConfig) IsValid() error {
+	if cfg.ControlPlaneTime <= 0 {
+		return fmt.Errorf("Config maintenace controlPlaneTime out is invalid")
+	}
+	if cfg.WorkerNodeTime <= 0 {
+		return fmt.Errorf("Config maintenace workerNodeTime is invalid")
+	}
+
+	return nil
+}
+
+func (cfg *maintenanceConfig) GetControlPlaneDuration() time.Duration {
+	return time.Duration(cfg.ControlPlaneTime) * time.Minute
+}
+
+func (cfg *maintenanceConfig) GetWorkerNodeDuration() time.Duration {
+	return time.Duration(cfg.WorkerNodeTime) * time.Minute
 }
 
 type scaleConfig struct {
@@ -25,11 +52,8 @@ type nodeDrain struct {
 }
 
 func (cfg *osdUpgradeConfig) IsValid() error {
-	if cfg.Maintenance.ControlPlaneTime <= 0 {
-		return fmt.Errorf("Config maintenace controlPlaneTime out is invalid")
-	}
-	if cfg.Maintenance.WorkerNodeTime <= 0 {
-		return fmt.Errorf("Config maintenace workerNodeTime is invalid")
+	if err := cfg.Maintenance.IsValid(); err != nil {
+		return err
 	}
 	if cfg.Scale.TimeOut <= 0 {
 		return fmt.Errorf("Config scale timeOut is invalid")
@@ -39,14 +63,6 @@ func (cfg *osdUpgradeConfig) IsValid() error {
 	}
 
 	return nil
-}
-
-func (cfg *osdUpgradeConfig) GetControlPlaneDuration() time.Duration {
-	return time.Duration(cfg.Maintenance.ControlPlaneTime) * time.Minute
-}
-
-func (cfg *osdUpgradeConfig) GetWorkerNodeDuration() time.Duration {
-	return time.Duration(cfg.Maintenance.WorkerNodeTime) * time.Minute
 }
 
 func (cfg *osdUpgradeConfig) GetScaleDuration() time.Duration {
