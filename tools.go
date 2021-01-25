25f312262dca34488b89b7a5f5c760f45ea9fbe2
Eric Fried <efried@redhat.com>
2021-01-25 11:05:23 -0600

    Post-boilerplate fixups
    Clean up make targets and obsolete scripts

--- tools.go
+++ tools.go
@@ -5,8 +5,5 @@
 package tools
 
 import (
-	_ "github.com/golang/mock/mockgen"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/onsi/ginkgo"
-	_ "k8s.io/code-generator/cmd/openapi-gen"
 )
