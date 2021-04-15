bf2540c55ad36d89aacf3168760f4f86d2bbb77e
Eric Fried <efried@redhat.com>
2021-04-15 14:10:49 -0500

    Boilerplate: Update to dbe75c938c50cc52b1f86ae3441aab0043b82f80
    Conventions:
    - openshift/golang-osd-operator: Update
    ---
    https://github.com/openshift/boilerplate/compare/d9d5abfd0745651959b5e4de82b751ec4c3a3dac...dbe75c938c50cc52b1f86ae3441aab0043b82f80
    
    commit: 272a910b67ee97c8085b1a9d8dd741f14d25f433
    author: Benjamin Dematteo
    Replacing REGISTRY_USER and REGISTRY_TOKEN bu QUAY_USER/QUAY_TOKEN for make docker-login
    
    commit: f49517a7e1623c641772ebde965bd60e5091ad2e
    author: Eric Fried
    Disable codecov
    
    ...while we investigate a [codecov.io security
    breach](https://about.codecov.io/security-update/)
    
    commit: d3c263cee468dc862b9bb108cd6e6c86297f3585
    author: Benjamin Dematteo
    Adding missing varaible for git token auhtent
    
    commit: 40db4e9aac5cae0b1fa5f71ca7170b762ef8ec5c
    author: Arjun Naik
    Remove flags for interactive docker run for the CSV generation
    
    Signed-off-by: Arjun Naik <anaik@redhat.com>
    
    commit: 38876608dfa38d552f368837b1e8faa5955aa215
    author: Eric Fried
    Fix CI: Support disabling SaaS file checks
    
    The 06-csv-generate test invokes (make targets that invoke)
    csv-generate.sh, which now queries the operator's SaaS file in
    app-interface to determine whether it's a hive or cluster operator.
    Since the test uses a dummy project, it has no such SaaS file in
    app-interface. Even if it did, the CI image doesn't have the right certs
    to query it over https.
    
    This commit enables an environment variable to bypass that whole section
    of csv-generate.sh, and enables it in the test case.
    
    commit: 380d825ff9e137cd6fccff62f2035872a6be3daf
    author: James Harrington
    Containerize yq
    
    commit: 0378b16b89cba33fecea0447f306eadd3b979d82
    author: James Harrington
    Ensure we fetch a valid deployed production hash
    
    commit: 5f1d9a464728641d0c5ea1828fed7b5b2361c99f
    author: Arjun Naik
    Small improvements to make it easier to run csv-generate test locally
    
    Signed-off-by: Arjun Naik <anaik@redhat.com>
    
    commit: b926c8703e255748b632d0dabdb061f439b581d4
    author: Arjun Naik
    Updated the hack generation script
    
    Signed-off-by: Arjun Naik <anaik@redhat.com>
    
    commit: 7973db3122b3f0bbade38cf95bc3ab1aa1b84e25
    author: Eric Fried
    Run CSV generator in a container
    
    ...unless we're already in a container.
    
    ...because we can't rely on py3 being present on jenkins nodes, and py2
    is dead.
    
    OSD-6993
    
    commit: 639160e9a92475f0a446c44aff3ad202d935e83e
    author: Arjun Naik
    Updated test case for CSV generation
    
    Signed-off-by: Arjun Naik <anaik@redhat.com>
    
    commit: 1dc94f0588ad8177e8804a8adf3bed94333453ec
    author: Arjun Naik
    Use trim_index and add default namespace for rolebinding subject SA
    
    Signed-off-by: Arjun Naik <anaik@redhat.com>
    
    commit: 2c6032115302c4449b8843b076a59d0cf2d25fa9
    author: Arjun Naik
    Add roles into CSV permissions
    
    Signed-off-by: Arjun Naik <anaik@redhat.com>

--- boilerplate/openshift/golang-osd-operator/csv-generate/common-generate-operator-bundle.py
+++ boilerplate/openshift/golang-osd-operator/csv-generate/common-generate-operator-bundle.py
@@ -94,6 +94,18 @@ class BindingsNotSupported(Exception):
             "to provided [Cluster]Roles. Found the following orphans:\n" +
             f"{bindings}")
 
+class UndefinedCSVNamespace(Exception):
+    def __init__(self, operator_name):
+        super().__init__(
+            f"Namespace not defined for operator {operator_name} in CSV template"
+        )
+
+class NoAssociatedRoleBinding(Exception):
+    def __init__(name, namespace):
+        super.__init__(
+            f"The Role {name}/{namespace} does not have an associated RoleBinding"
+        )
+
 def log_resource(resource):
     """Log a message that we're processing the given resource.
 
@@ -238,6 +250,36 @@ for kind, csvkey, binding_map in rolekind_csvkey_bindingmap:
             })
     # Get rid of these so we can iterate over what's left at the end
     trim_index(by_kind, kind, 'ALL')
+
+
+csv['spec']['install']['spec']['permissions'] = []
+
+if 'namespace' not in csv['metadata']:
+    raise UndefinedCSVNamespace(OPERATOR_NAME)
+
+# Find namespace of operator from CSV template
+namespace = csv['metadata']['namespace']
+
+if 'Role' in by_kind:
+    for role in by_kind['Role']:
+        # We assume there is always a rolebinding of defined role
+        if role['metadata']['name'] not in rb_by_role:
+            raise NoAssociatedRoleBinding(role['metadata']['name'], role['metadata']['namespace'])
+        role_binding = rb_by_role[role['metadata']['name']]
+
+        # If the RoleBinding subject is a ServiceAccount in the same namespace as the operator
+        # we add it to CSV and remove it from the by_kind dict
+        if len(role_binding['subjects']) == 1 and \
+            role_binding['subjects'][0]['kind'] == 'ServiceAccount' and \
+            role_binding['subjects'][0].get('namespace', namespace) == namespace:
+                csv['spec']['install']['spec']['permissions'].append(
+                    {
+                        'rules': role['rules'],
+                        'serviceAccountName': role_binding['subjects'][0]['name'] 
+                    }
+                )
+                trim_index(by_kind, 'Role', role)
+                trim_index(by_kind, 'RoleBinding', role_binding)
 
 ## Add the Deployment
 # We already made sure there's exactly one Deployment
