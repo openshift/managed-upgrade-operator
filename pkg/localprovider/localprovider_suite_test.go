0f49680f33beef5784724851d254ccd2d9c9f31b
Ravi Trivedi <travi@redhat.com>
2021-06-07 10:51:41 +0530

    OSD-7095 - Adding localprovider tests

---
+++ pkg/localprovider/localprovider_suite_test.go
@@ -0,0 +1,13 @@
+package localprovider_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestLocalprovider(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Localprovider Suite")
+}
