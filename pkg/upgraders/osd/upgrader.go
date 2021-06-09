71e6c37dc744e644f0c119cc55537dc31c2208d9
Bo Meng <bmeng@redhat.com>
2021-06-09 12:53:51 +0800

    make the namespace in health check configurable

--- pkg/upgraders/osd/upgrader.go
+++ pkg/upgraders/osd/upgrader.go
@@ -584,7 +584,15 @@ func performClusterHealthCheck(c client.Client, metricsClient metrics.Metrics, c
 	if len(ic) > 0 {
 		icQuery = `,alertname!="` + strings.Join(ic, `",alertname!="`) + `"`
 	}
-	healthCheckQuery := `ALERTS{alertstate="firing",severity="critical",namespace=~"^openshift.*|^kube-.*|^default$",namespace!="openshift-customer-monitoring",namespace!="openshift-logging",namespace!="openshift-operators"` + icQuery + "}"
+
+	ignoredNamespace := cfg.HealthCheck.IgnoredNamespaces
+	ignoredNamespaceQuery := ""
+	if len(ignoredNamespace) > 0 {
+		ignoredNamespaceQuery = `,namespace!="` + strings.Join(ignoredNamespace, `",namespace!="`) + `"`
+	}
+
+	healthCheckQuery := `ALERTS{alertstate="firing",severity="critical",namespace=~"^openshift.*|^kube-.*|^default$"` + ignoredNamespaceQuery + icQuery + "}"
+
 	alerts, err := metricsClient.Query(healthCheckQuery)
 	if err != nil {
 		return false, fmt.Errorf("unable to query critical alerts: %s", err)
