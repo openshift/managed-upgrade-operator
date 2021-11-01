769bb1c77b96560a87345af5ef72f61fa8a22bfb
Aniket Joshi <anjoshi@redhat.com>
2021-11-01 17:30:03 +0530

    OSD-8404 fixin version comparison in upgradeable.go
    Signed-off-by: Aniket Joshi <anjoshi@redhat.com>

--- pkg/upgraders/upgradeable.go
+++ pkg/upgraders/upgradeable.go
@@ -3,7 +3,6 @@ package upgraders
 import (
 	"context"
 	"fmt"
-	"strings"
 
 	"github.com/blang/semver"
 	"github.com/go-logr/logr"
@@ -12,7 +11,10 @@ import (
 	cv "github.com/openshift/managed-upgrade-operator/pkg/clusterversion"
 )
 
-func (c *clusterUpgrader) CheckUpgradeStatus(ctx context.Context, logger logr.Logger) (bool, error) {
+func (c *clusterUpgrader) IsUpgradeable(ctx context.Context, logger logr.Logger) (bool, error) {
+	cV := &configv1.ClusterVersion{}
+	uC := &upgradev1alpha1.UpgradeConfig{}
+
 	//Check if the cluster is already upgrading -
 	upgradeCommenced, err := c.cvClient.HasUpgradeCommenced(c.upgradeConfig)
 	if err != nil {
@@ -23,20 +25,6 @@ func (c *clusterUpgrader) CheckUpgradeStatus(ctx context.Context, logger logr.Lo
 		return true, nil
 	}
 
-	//Pull the cluster-version - cvclient / get clusterversion
-	//If Upgradable is not present at all then return "True" and proceed with the upgrade.
-	//Upgradable = True/False
-	// type: Upgradable && status: "True"/"False"
-
-	/* If Upgradable = "True" then return true
-	If Upgradable = "False" then
-		1. check if the cluster upgrade is z-stream then return true and if it's y-stream then return false
-	*/
-
-	return false, nil
-}
-
-func (c *clusterUpgrader) IsUpgradable(ctx context.Context, uC *upgradev1alpha1.UpgradeConfig, cV *configv1.ClusterVersion, logger logr.Logger) (bool, error) {
 	// get current clusterversion
 	cvVersion, err := cv.GetCurrentVersion(cV)
 	if err != nil {
@@ -50,6 +38,7 @@ func (c *clusterUpgrader) IsUpgradable(ctx context.Context, uC *upgradev1alpha1.
 		return false, err
 	}
 
+	// get the desired clusterversion from upgradeconfig
 	ucVersion := uC.Spec.Desired.Version
 	if err != nil {
 		logger.Error(err, "failed to get current upgrade config desired version")
@@ -62,13 +51,7 @@ func (c *clusterUpgrader) IsUpgradable(ctx context.Context, uC *upgradev1alpha1.
 		return false, err
 	}
 
-	var vercompareidx int = 1
-
-	cVversionAarray := strings.Split(cvVersion, ".") //cannot use parsedCvVersion - cannot use parsedCvVersion (variable of type semver.Version) as string value in argument to strings.Split
-	uCversionAarray := strings.Split(ucVersion, ".")
-
 	// check if the clusterversion has "Upgradeable" else return true
-	// logic problem - fix the loop
 	for _, val := range cV.Status.Conditions {
 		if val.Type != configv1.OperatorUpgradeable {
 			return true, nil
@@ -80,13 +63,12 @@ func (c *clusterUpgrader) IsUpgradable(ctx context.Context, uC *upgradev1alpha1.
 			return true, nil
 		} else if condition.Type == configv1.OperatorUpgradeable && condition.Status == configv1.ConditionFalse {
 			// if the upgradeable is false then we need to check the current version with upgrade version for y-stream update
-			if cVversionAarray[vercompareidx] == uCversionAarray[vercompareidx] {
-				return true, nil
-			} else {
+			if parsedUcVersion.Major >= parsedCvVersion.Major && parsedUcVersion.Minor > parsedCvVersion.Minor {
 				return false, err
+			} else if parsedUcVersion.Major >= parsedCvVersion.Major && parsedUcVersion.Minor == parsedCvVersion.Minor {
+				return true, nil
 			}
 		}
 	}
-
 	return false, nil
 }
--- pkg/upgraders/upgradeable_test.go
+++ pkg/upgraders/upgradeable_test.go
@@ -2,69 +2,99 @@ package upgraders
 
 import (
 	"context"
-	"testing"
 
-	"github.com/docker/docker/client"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
 	"github.com/go-logr/logr"
-	configv1 "github.com/openshift/api/config/v1"
+	"github.com/golang/mock/gomock"
+	"k8s.io/apimachinery/pkg/types"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+
 	upgradev1alpha1 "github.com/openshift/managed-upgrade-operator/pkg/apis/upgrade/v1alpha1"
-	cv "github.com/openshift/managed-upgrade-operator/pkg/clusterversion"
+	cvMocks "github.com/openshift/managed-upgrade-operator/pkg/clusterversion/mocks"
+	mockDrain "github.com/openshift/managed-upgrade-operator/pkg/drain/mocks"
+	emMocks "github.com/openshift/managed-upgrade-operator/pkg/eventmanager/mocks"
+	mockMachinery "github.com/openshift/managed-upgrade-operator/pkg/machinery/mocks"
+	mockMaintenance "github.com/openshift/managed-upgrade-operator/pkg/maintenance/mocks"
+	mockMetrics "github.com/openshift/managed-upgrade-operator/pkg/metrics/mocks"
+	mockScaler "github.com/openshift/managed-upgrade-operator/pkg/scaler/mocks"
+	"github.com/openshift/managed-upgrade-operator/util/mocks"
+	testStructs "github.com/openshift/managed-upgrade-operator/util/mocks/structs"
 )
 
-func Test_clusterUpgrader_IsUpgradable(t *testing.T) {
-	type fields struct {
-		steps                []upgradesteps.UpgradeStep
-		client               client.Client
-		metrics              metrics.Metrics
-		cvClient             cv.ClusterVersion
-		notifier             eventmanager.EventManager
-		scaler               scaler.Scaler
-		availabilityCheckers ac.AvailabilityCheckers
-		upgradeConfig        *upgradev1alpha1.UpgradeConfig
-		drainstrategyBuilder drain.NodeDrainStrategyBuilder
-		maintenance          maintenance.Maintenance
-		machinery            machinery.Machinery
-		config               *upgraderConfig
-	}
-	type args struct {
-		ctx    context.Context
-		uC     *upgradev1alpha1.UpgradeConfig
-		cV     *configv1.ClusterVersion
+var _ = Describe("UpgradableCheckStep", func() {
+	var (
 		logger logr.Logger
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    bool
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &clusterUpgrader{
-				steps:                tt.fields.steps,
-				client:               tt.fields.client,
-				metrics:              tt.fields.metrics,
-				cvClient:             tt.fields.cvClient,
-				notifier:             tt.fields.notifier,
-				scaler:               tt.fields.scaler,
-				availabilityCheckers: tt.fields.availabilityCheckers,
-				upgradeConfig:        tt.fields.upgradeConfig,
-				drainstrategyBuilder: tt.fields.drainstrategyBuilder,
-				maintenance:          tt.fields.maintenance,
-				machinery:            tt.fields.machinery,
-				config:               tt.fields.config,
-			}
-			got, err := c.IsUpgradable(tt.args.ctx, tt.args.uC, tt.args.cV, tt.args.logger)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("clusterUpgrader.IsUpgradable() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got != tt.want {
-				t.Errorf("clusterUpgrader.IsUpgradable() = %v, want %v", got, tt.want)
-			}
+		// mocks
+		mockKubeClient           *mocks.MockClient
+		mockCtrl                 *gomock.Controller
+		mockMaintClient          *mockMaintenance.MockMaintenance
+		mockScalerClient         *mockScaler.MockScaler
+		mockMachineryClient      *mockMachinery.MockMachinery
+		mockMetricsClient        *mockMetrics.MockMetrics
+		mockCVClient             *cvMocks.MockClusterVersion
+		mockDrainStrategyBuilder *mockDrain.MockNodeDrainStrategyBuilder
+		mockEMClient             *emMocks.MockEventManager
+		// upgradeconfig to be used during tests
+		upgradeConfigName types.NamespacedName
+		upgradeConfig     *upgradev1alpha1.UpgradeConfig
+
+		// upgrader to be used during tests
+		config   *upgraderConfig
+		upgrader *osdUpgrader
+	)
+
+	BeforeEach(func() {
+		upgradeConfigName = types.NamespacedName{
+			Name:      "test-upgradeconfig",
+			Namespace: "test-namespace",
+		}
+		upgradeConfig = testStructs.NewUpgradeConfigBuilder().WithNamespacedName(upgradeConfigName).GetUpgradeConfig()
+		mockCtrl = gomock.NewController(GinkgoT())
+		mockKubeClient = mocks.NewMockClient(mockCtrl)
+		mockMaintClient = mockMaintenance.NewMockMaintenance(mockCtrl)
+		mockMetricsClient = mockMetrics.NewMockMetrics(mockCtrl)
+		mockScalerClient = mockScaler.NewMockScaler(mockCtrl)
+		mockMachineryClient = mockMachinery.NewMockMachinery(mockCtrl)
+		mockCVClient = cvMocks.NewMockClusterVersion(mockCtrl)
+		mockDrainStrategyBuilder = mockDrain.NewMockNodeDrainStrategyBuilder(mockCtrl)
+		mockEMClient = emMocks.NewMockEventManager(mockCtrl)
+		logger = logf.Log.WithName("cluster upgrader test logger")
+		config = buildTestUpgraderConfig(90, 30, 8, 120, 30)
+		upgrader = &osdUpgrader{
+			clusterUpgrader: &clusterUpgrader{
+				client:               mockKubeClient,
+				metrics:              mockMetricsClient,
+				cvClient:             mockCVClient,
+				notifier:             mockEMClient,
+				config:               config,
+				scaler:               mockScalerClient,
+				drainstrategyBuilder: mockDrainStrategyBuilder,
+				maintenance:          mockMaintClient,
+				machinery:            mockMachineryClient,
+				upgradeConfig:        upgradeConfig,
+			},
+		}
+	})
+
+	AfterEach(func() {
+		mockCtrl.Finish()
+	})
+
+	Context("When running the upgradable-check phase", func() {
+		Context("With Upgradeable flag present in csv status condition", func() {
+			Context("When current 'y' stream version is lower then upgrade version", func() {
+				It("will not perform upgrade", func() {
+					// gomock.InOrder(
+					// mockCVClient.EXPECT().HasUpgradeCommenced(gomock.Any()).Return(true, nil),
+					// )
+					result, err := upgrader.IsUpgradeable(context.TODO(), logger)
+					Expect(err).NotTo(HaveOccurred())
+					Expect(result).To(BeTrue())
+				})
+			})
+
 		})
-	}
-}
+	})
+})
