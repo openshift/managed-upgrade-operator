8d7c5f4df237faabc4bfa0f80b9711ce1433f2f9
Pete Brown <rendhalver@users.noreply.github.com>
2022-06-17 15:26:31 +1000

    Use more descriptive variable names

--- pkg/clusterversion/cv.go
+++ pkg/clusterversion/cv.go
@@ -275,8 +275,8 @@ func (c *clusterVersionClient) runUpgradeWithImage(cv *configv1.ClusterVersion, 
 
 	if cv.Spec.DesiredUpdate == nil || cv.Spec.DesiredUpdate.Image != desired.Image {
 		logger.Info(fmt.Sprintf("Setting ClusterVersion to Image %s", desired.Image))
-		patch := []byte(fmt.Sprintf(`{"spec":{"desiredUpdate":{"image":"%s","version":null}}}`, desired.Image))
-		err := c.client.Patch(context.TODO(), cv, client.RawPatch(types.MergePatchType, patch))
+		desiredImage := []byte(fmt.Sprintf(`{"spec":{"desiredUpdate":{"image":"%s","version":null}}}`, desired.Image))
+		err := c.client.Patch(context.TODO(), cv, client.RawPatch(types.MergePatchType, desiredImage))
 		if err != nil {
 			return false, err
 		}
@@ -289,8 +289,8 @@ func (c *clusterVersionClient) runUpgradeWithChannelVersion(cv *configv1.Cluster
 
 	if cv.Spec.Channel != desired.Channel {
 		logger.Info(fmt.Sprintf("Setting ClusterVersion to Channel %s Version %s", desired.Channel, desired.Version))
-		patch := []byte(fmt.Sprintf(`{"spec":{"channel":"%s"}}`, desired.Channel))
-		err := c.client.Patch(context.TODO(), cv, client.RawPatch(types.MergePatchType, patch))
+		desiredChannel := []byte(fmt.Sprintf(`{"spec":{"channel":"%s"}}`, desired.Channel))
+		err := c.client.Patch(context.TODO(), cv, client.RawPatch(types.MergePatchType, desiredChannel))
 		if err != nil {
 			return false, err
 		}
@@ -314,8 +314,8 @@ func (c *clusterVersionClient) runUpgradeWithChannelVersion(cv *configv1.Cluster
 	}
 
 	cv.Spec.Overrides = []configv1.ComponentOverride{}
-	patch := []byte(fmt.Sprintf(`{"spec":{"desiredUpdate":{"version":"%s","image":null}}}`, desired.Version))
-	err := c.client.Patch(context.TODO(), cv, client.RawPatch(types.MergePatchType, patch))
+	desiredVersion := []byte(fmt.Sprintf(`{"spec":{"desiredUpdate":{"version":"%s","image":null}}}`, desired.Version))
+	err := c.client.Patch(context.TODO(), cv, client.RawPatch(types.MergePatchType, desiredVersion))
 	if err != nil {
 		return false, err
 	}
