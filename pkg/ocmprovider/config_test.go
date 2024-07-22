74e24d22d36a50a3bdcd477aac461fba48c4d367
Anwardeen A <anwa@redhat.com>
2024-07-22 10:32:31 +0530

    rebased

--- pkg/ocmprovider/config_test.go
+++ pkg/ocmprovider/config_test.go
@@ -10,14 +10,13 @@ const baseUrl = "http://test.ocp"
 
 var _ = Describe("Config", func() {
 	var (
-		mockCtrl *gomock.Controller
-
+		mockCtrl    *gomock.Controller
 		ocmProvider OcmProviderConfig
 	)
 
 	BeforeEach(func() {
 		mockCtrl = gomock.NewController(GinkgoT())
-
+		// Initialing temp value for config's baseurl
 		ocmProvider = OcmProviderConfig{
 			ConfigManager: ConfigManager{
 				OcmBaseUrl: baseUrl,
@@ -30,10 +29,12 @@ var _ = Describe("Config", func() {
 	})
 
 	Context("GetOCMBaseURL", func() {
-		It("Valid", func() {
+		// Validating IsValid func
+		It("Testing the IsValid function", func() {
 			err := ocmProvider.IsValid()
 			Expect(err).To(BeNil())
 		})
+		// Validating GetOCMBaseURL func
 		It("Get base url", func() {
 			url := ocmProvider.GetOCMBaseURL()
 			Expect(url).To(Not(BeNil()))
