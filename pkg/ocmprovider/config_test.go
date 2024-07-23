e5b8c694787521c0132ae981a9dfb9c2d016e7f6
Anwardeen A <anwa@redhat.com>
2024-07-23 13:56:51 +0530

    added test case

--- pkg/ocmprovider/config_test.go
+++ pkg/ocmprovider/config_test.go
@@ -40,6 +40,16 @@ var _ = Describe("Config", func() {
 			Expect(url).To(Not(BeNil()))
 		})
 
+		// Validating IsValid func
+		It("Testing the IsValid function error", func() {
+			ocmProviderErr := OcmProviderConfig{
+				ConfigManager: ConfigManager{
+					OcmBaseUrl: " http://example.com/a/b/c/d/e/f/g/h/i/j/k/l/m/n/o/p/q/r/s/t/u/v/w/x/y/z",
+				},
+			}
+			err := ocmProviderErr.IsValid()
+			Expect(err).To(HaveOccurred())
+		})
 	})
 
 })
