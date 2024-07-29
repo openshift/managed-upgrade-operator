07da8a936cce74814bca1caa366193d3a2b0019d
Anwardeen A <anwa@redhat.com>
2024-07-29 14:05:51 +0530

    Added test case for lognotifier

---
+++ pkg/notifier/lognotifier_test.go
@@ -0,0 +1,42 @@
+package notifier
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"go.uber.org/mock/gomock"
+)
+
+const (
+	value       = "StateCompleted"
+	description = "Testing"
+)
+
+var _ = Describe("logNotifier", func() {
+	var (
+		mockCtrl *gomock.Controller
+		lNotify  logNotifier
+	)
+	BeforeEach(func() {
+		mockCtrl = gomock.NewController(GinkgoT())
+
+	})
+	AfterEach(func() {
+		mockCtrl.Finish()
+	})
+
+	Context("LogNotifier", func() {
+		// Test NewLogNotifier returns a new logNotifier
+		It("NewLogNotifier return struct", func() {
+			//lNotify = logNotifier{}
+			logNotifier, err := NewLogNotifier()
+			Expect(err).To(BeNil())
+			Expect(logNotifier).To(Equal(&lNotify))
+		})
+
+		// Testing NotifyState func
+		It("Testing NotifyState func", func() {
+			err := lNotify.NotifyState(value, description)
+			Expect(err).To(BeNil())
+		})
+	})
+})
