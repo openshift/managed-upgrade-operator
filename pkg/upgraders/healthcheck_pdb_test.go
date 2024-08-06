b20041ed8b54016038eae97fd50b00045595bec3
Shital Jante <sjante@redhat.com>
2024-08-06 12:38:25 +0530

    unit test case

--- pkg/upgraders/healthcheck_pdb_test.go
+++ pkg/upgraders/healthcheck_pdb_test.go
@@ -22,13 +22,14 @@ import (
 
 var _ = Describe("checkPodDisruptionBudgets", func() {
 	var (
-		mockClient        *mocks.MockClient
-		mockCtrl          *gomock.Controller
-		logger            logr.Logger
-		mockMetricsClient *mockMetrics.MockMetrics
-		upgradeConfigName types.NamespacedName
-		upgradeConfig     *upgradev1alpha1.UpgradeConfig
-		mockdvoclientbulder     		*dvoMocks.MockDvoClientBuilder
+		mockClient          *mocks.MockClient
+		mockCtrl            *gomock.Controller
+		logger              logr.Logger
+		mockMetricsClient   *mockMetrics.MockMetrics
+		upgradeConfigName   types.NamespacedName
+		upgradeConfig       *upgradev1alpha1.UpgradeConfig
+		mockdvoclientbulder *dvoMocks.MockDvoClientBuilder
+		mockdvoclient       *dvoMocks.MockDvoClient
 	)
 
 	BeforeEach(func() {
@@ -36,6 +37,7 @@ var _ = Describe("checkPodDisruptionBudgets", func() {
 		mockClient = mocks.NewMockClient(mockCtrl)
 		mockMetricsClient = mockMetrics.NewMockMetrics(mockCtrl)
 		mockdvoclientbulder = dvoMocks.NewMockDvoClientBuilder(mockCtrl)
+		mockdvoclient = dvoMocks.NewMockDvoClient(mockCtrl)
 		logger = logf.Log.WithName("cluster upgrader test logger")
 		upgradeConfigName = types.NamespacedName{
 			Name:      "test-upgradeconfig",
@@ -124,7 +126,7 @@ var _ = Describe("checkPodDisruptionBudgets", func() {
 		})
 	})
 
-	Context("When there is invalid PDB configuration", func() {
+	Context("When there is invalid PDB configuration incorrect MaxUnavailable", func() {
 		It("Prehealth PDB check will fail", func() {
 			pdbList := &policyv1.PodDisruptionBudgetList{
 				Items: []policyv1.PodDisruptionBudget{
@@ -137,18 +139,6 @@ var _ = Describe("checkPodDisruptionBudgets", func() {
 							MaxUnavailable: &intstr.IntOrString{
 								Type:   intstr.Int,
 								IntVal: 0,
-							},
-						},
-					},
-					{
-						ObjectMeta: metav1.ObjectMeta{
-							Namespace: "openshift-operators",
-							Name:      "pdb-2",
-						},
-						Spec: policyv1.PodDisruptionBudgetSpec{
-							MinAvailable: &intstr.IntOrString{
-								Type:   intstr.String,
-								StrVal: "100%",
 							},
 						},
 					},
@@ -205,7 +195,21 @@ var _ = Describe("checkPodDisruptionBudgets", func() {
 		})
 	})
 
-	Context("checkNamespaceExistsInArray", func() {
+	Context("When DVO service is down", func() {
+		It("checkDvoMetrics check will fail", func() {
+
+			gomock.InOrder(
+				mockdvoclientbulder.EXPECT().New(gomock.Any()).Return(mockdvoclient, nil),
+				mockdvoclient.EXPECT().GetMetrics().Return(nil, fmt.Errorf("Fake cannot fetch all metrics ")),
+				mockMetricsClient.EXPECT().UpdateMetricHealthcheckFailed(upgradeConfig.Name, metrics.DvoMetricsQueryFailed),
+			)
+			reason, err := checkDvoMetrics(mockClient, mockdvoclientbulder, logger)
+			Expect(err).To(HaveOccurred())
+			Expect(reason).To(Equal(metrics.DvoMetricsQueryFailed))
+		})
+	})
+
+	Context("When namespace doesn't exists in array checkNamespaceExistsInArray", func() {
 		It("checkNamespaceExistsInArray fail", func() {
 
 			result := checkNamespaceExistsInArray(namespaceException, "openshift-logging-1")
@@ -213,11 +217,36 @@ var _ = Describe("checkPodDisruptionBudgets", func() {
 		})
 	})
 
-	Context("checkNamespaceExistsInArray", func() {
-		It("checkNamespaceExistsInArray fail", func() {
+	Context("When namespace exists in array checkNamespaceExistsInArray", func() {
+		It("checkNamespaceExistsInArray success", func() {
 
 			result := checkNamespaceExistsInArray(namespaceException, "openshift-logging")
 			Expect(result).To(Equal(true))
+		})
+	})
+
+	Context("When there is an error getting DVO metrics", func() {
+		It("should return DvoMetricsQueryFailed", func() {
+			
+			gomock.InOrder(
+				mockdvoclientbulder.EXPECT().New(gomock.Any()).Return(mockdvoclient, nil),
+			mockdvoclient.EXPECT().GetMetrics().Return(nil, fmt.Errorf("failed to get DVO metrics")),
+			)
+			reason, err := checkDvoMetrics(mockClient, mockdvoclientbulder, logger)
+			Expect(err).To(HaveOccurred())
+			Expect(reason).To(Equal(metrics.DvoMetricsQueryFailed))
+		})
+	})
+
+	Context("When there is an error creating DVO client", func() {
+		It("should return DvoClientCreationFailed", func() {
+			gomock.InOrder(
+				
+			mockdvoclientbulder.EXPECT().New(mockClient).Return(nil, fmt.Errorf("failed to create DVO client")),
+			)
+			reason, err := checkDvoMetrics(mockClient, mockdvoclientbulder, logger)
+			Expect(err).To(HaveOccurred())
+			Expect(reason).To(Equal(metrics.DvoClientCreationFailed))
 		})
 	})
 })
