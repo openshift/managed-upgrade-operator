94c413d40622a531f4216eb90d622137ef0c194b
Chamal Abeywardhana <cabeywar@redhat.com>
2026-06-29 07:30:09 +1000

    Boilerplate update

--- boilerplate/generated-includes.mk
+++ boilerplate/generated-includes.mk
@@ -2,8 +2,3 @@
 # This file automatically includes any *.mk files in your subscribed
 # conventions. Please ensure your base Makefile includes only this file.
 include boilerplate/_lib/boilerplate.mk
-include boilerplate/openshift/golang-osd-operator/csv-generate/csv-generate.mk
-include boilerplate/openshift/golang-osd-operator/project.mk
-include boilerplate/openshift/golang-osd-operator/standard.mk
-include boilerplate/openshift/golang-osd-e2e/project.mk
-include boilerplate/openshift/golang-osd-e2e/standard.mk
